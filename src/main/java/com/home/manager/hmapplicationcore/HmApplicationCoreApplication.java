d2e708bea0408b86f0d3a78e06e9a52b618ab9cd
CaioCaminha <caminhacaiopro@gmail.com>
2022-02-08 14:48:47 -0300

    First Commit

---
+++ src/main/java/com/home/manager/hmapplicationcore/HmApplicationCoreApplication.java
@@ -0,0 +1,13 @@
+package com.home.manager.hmapplicationcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmApplicationCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmApplicationCoreApplication.class, args);
+	}
+
+}
