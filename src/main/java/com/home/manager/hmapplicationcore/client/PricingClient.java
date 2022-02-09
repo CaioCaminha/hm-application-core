d8e4877ebe6ad81db853632abbaf5b47f04439c3
CaioCaminha <caminhacaiopro@gmail.com>
2022-02-09 09:41:03 -0300

    Implementing princing client

--- src/main/java/com/home/manager/hmapplicationcore/client/PricingClient.java
+++ src/main/java/com/home/manager/hmapplicationcore/client/PricingClient.java
@@ -1,4 +1,18 @@
 package com.home.manager.hmapplicationcore.client;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
 public class PricingClient {
+
+    private final WebClient webClient;
+
+    public PricingClient(WebClient webClient){
+        this.webClient = webClient;
+    }
+
+
+
+
 }
